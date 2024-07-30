785be1317885f44575e164c4785d5eceabdd8812
faisal1 <khanfaisal900413@gmail.com>
2024-07-30 15:56:06 +0530

    faisal4

--- 2_datatypes.js
+++ 2_datatypes.js
@@ -1,3 +1,53 @@
-// alert(3+2) we are using js not browser showing erroe
-let a=console.log(3+3);
-console.log(typeof(6));
+// // // alert(3+2) we are using js not browser showing erroe
+// let a=console.log(3+3);
+// console.log(typeof(6));
+
+// //primitive type
+// // srting ,  number , boolean , null , undefined , symbol,bigint
+// //dynamic language 
+// const score=100
+// const score2=10.4
+// console.log(typeof score);
+// console.log(typeof score2);
+// // refrence type /non primitive
+// array , objects , functions 
+// const array=["faisal", "khan"]
+// let objects={
+//     age:22,
+//     name:"faisal",
+// }
+// const myfuction=function(){
+//     console.log("faisal khan");
+// }
+
+// console.log(typeof array)
+// console.log(typeof objects)
+
+// const arr2=[1,2,3]
+// console.log(typeof arr2);
+
+// console.log(typeof myfuction);
+
+
+// stack(primitive)  , heap(non-primitive)
+
+let f1="khanfaisalzafirulhasan"
+let f2=f1
+
+f2="khanfahad"
+console.log(f1);
+console.log(f2);
+
+
+let obj1={
+    user:"faisal",
+    password:"123"
+}
+console.log(obj1);
+let obj2=obj1
+
+console.log(obj2)
+obj2.user="fahad"
+obj2.password="234"
+console.log(obj1);
+console.log(obj2);
--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 console.log("hello world")
+
+let srting="faisal"
+let char=50
+console.log(`hello my name is ${srting} and my repo count is ${char}`);
+const gamename=new String("khan")
+console.log(gamename[2]);
